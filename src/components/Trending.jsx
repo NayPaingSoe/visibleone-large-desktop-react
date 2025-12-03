8e05c7f971e82d7669b847dadefd03e75e2bb0f5
NayPaingSoe <naypaingsoe042@gmail.com>
2025-12-03 16:47:39 +0630

    feat(ui): replace text arrows with svg icons in multiple components
    Replace simple text arrows with styled SVG icons in Sidebar, Trending, and HappeningNow compone

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -31,8 +31,8 @@ const Trending = () => {
         <div className="section-header">
           <h2 className="section-title">Trending</h2>
           <div className="arrows">
-            <span className="arrow-left">&lt;</span>
-            <span className="arrow-right">&gt;</span>
+            <span className="arrow-left"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="#554f4f" d="m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6z"></path></svg></span>
+            <span className="arrow-right"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="#554f4f" d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6z"></path></svg></span>
           </div>
         </div>
 
